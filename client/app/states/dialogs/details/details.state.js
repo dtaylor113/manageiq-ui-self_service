ManageIQ/manageiq-ui-service

Created dialog editing state

client/app/states/dialogs/details/details.state.js
@@ -32,9 +32,30 @@
   }
 
   /** @ngInject */
-  function StateController(dialog) {
+  function StateController($state, dialog, CollectionsApi, Notifications) {
     var vm = this;
 
     vm.dialog = dialog;
+    vm.dialog.performAction = performAction;
+    vm.dialog.removeDialog = removeDialog;
+
+    function performAction(item) {
+      $state.go('dialogs.edit', {dialogId: dialog.id});
+    }
+
+    function removeDialog() {
+      // TODO blindly-programmed, check after CRUD for service_dialog is done
+      var removeAction = {action: 'remove'};
+      CollectionsApi.post('service_dialogs', vm.dialog.id, {}, removeAction).then(removeSuccess, removeFailure);
+
+      function removeSuccess() {
+        Notifications.success(vm.dialog.name + __(' was removed.'));
+        $state.go('dialogs.list');
+      }
+
+      function removeFailure(data) {
+        Notifications.error(__('There was an error removing this dialog.'));
+      }
+    }
   }
 })();

dtaylor113/manageiq-ui-self_service

client/app/states/dialogs/details/details.state.js
@@ -32,9 +32,29 @@
   }
 
   /** @ngInject */
-  function StateController(dialog) {
+  function StateController($state, dialog, CollectionsApi, Notifications) {
     var vm = this;
 
     vm.dialog = dialog;
+    vm.dialog.performAction = performAction;
+    vm.dialog.removeDialog = removeDialog;
+
+    function performAction(item) {
+      $state.go('dialogs.edit', {dialogId: dialog.id});
+    }
+
+    function removeDialog() {
+      var removeAction = {action: 'delete'};
+      CollectionsApi.post('service_dialogs', vm.dialog.id, {}, removeAction).then(removeSuccess, removeFailure);
+
+      function removeSuccess() {
+        Notifications.success(vm.dialog.name + __(' was removed.'));
+        $state.go('dialogs.list');
+      }
+
+      function removeFailure(data) {
+        Notifications.error(__('There was an error removing this dialog.'));
+      }
+    }
   }
 })();